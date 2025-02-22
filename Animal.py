42609b80eb6c5125576ed983d6d83eadbcbf0199
Feruza29 <feruza29@me.com>
2025-02-21 23:05:36 -0500

    Update Usage.ipynb

--- Animal.py
+++ Animal.py
@@ -6,3 +6,20 @@ class Animal:
     def talk(self):
         print("hi")
 
+from Animal import Animal
+
+# Creating an Animal instance
+x = Animal("Leo", "Lion")
+
+x.talk()
+x.eat("meat")
+x.sleep(5)
+x.walk(2)
+x.describe()
+x.set_name("Kile")
+x.descibe()
+x.make_sound("Roar")
+x.play("chasing a butterfly")
+x.age(4)
+x.run(20)
+x.jump(3)
